0870fe7055c57d2b4029cef636376c00d56ffb5e
Arif Rizqi Yulianto <arifrizqiyulianto@gmail.com>
2026-08-05 09:36:07 +0700

    feat(core): add build configuration

---
+++ src/core/config_build.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_BUILD_H
+#define CONFIG_BUILD_H
+
+//==================================================
+// Firmware Information
+//==================================================
+
+#define FW_NAME        "SIMIPAL IoT LABKESDA"
+#define FW_VERSION     "1.0.0"
+
+//==================================================
+// Build Information
+//==================================================
+
+#define BUILD_DATE     __DATE__
+#define BUILD_TIME     __TIME__
+
+#endif // CONFIG_BUILD_H
