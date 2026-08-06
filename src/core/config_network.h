564de0ff6cf321780f56c12d12b4fd34a54a050d
Arif Rizqi Yulianto <arifrizqiyulianto@gmail.com>
2026-08-06 14:50:53 +0700

    feat(network): add WiFi configuration and manager skeleton

---
+++ src/core/config_network.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_NETWORK_H
+#define CONFIG_NETWORK_H
+
+#include <stdint.h>
+
+constexpr char WIFI_SSID[] = "YOUR_WIFI_SSID";
+
+constexpr char WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
+
+    constexpr uint32_t WIFI_CONNECT_TIMEOUT_MS = 15000;
+
+#endif // CONFIG_NETWORK_H
